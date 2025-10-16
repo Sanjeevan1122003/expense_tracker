27d8e75dd9fcf404f3ef733b268bf1a091d5899f
sanjeevan <sanjeevan1122003@users.noreply.github.com>
2025-10-16 11:02:09 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mysql = require("mysql2");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 
 
