e070b804514137fc6e41c066244b7459901fde8c
sanjeevan <145178642+Sanjeevan1122003@users.noreply.github.com>
2025-11-17 22:27:17 +0530

    Update api.jsx

--- frontend/src/lib/api.jsx
+++ frontend/src/lib/api.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://expense-tracker-zeta-woad.vercel.app/",
     withCredentials: true, // send/receive cookies
 });
 
